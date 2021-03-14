97895d3090c328053f961f3546050b1a70fad9a8
Nick Brook <nrbrook@gmail.com>
2021-03-14 19:34:38 +0000

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "4.4.0-b.2"
+let checksum = "f271f0c0ff27f38b5da0b7a48c939aebb0e9b10c9d0682133e4605a64b4768b5"
+
+let package = Package(
+    name: "AirTurnUI",
+    platforms: [.iOS(.v10), .macOS(.v10.14)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AirTurnUI",
+            targets: ["AirTurnUITargets"]
+        ),
+    ],
+    dependencies: [
+        .package(name: "AirTurnInterface",
+                         url: "https://github.com/AirTurn/AirTurnInterface-static-SP",
+                         from: Version(version)
+        )
+    ],
+    targets: [
+        .binaryTarget(name: "AirTurnUI", url: "https://airturn.com/framework/AirTurnUI.\(version).static.spm.zip", checksum: checksum),
+        .target(name: "AirTurnUITargets",
+                        dependencies: [
+                            .target(name: "AirTurnUI"),
+                            .product(name: "AirTurnInterface", package: "AirTurnInterface")
+                        ],
+                        path: "Sources"
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# AirTurnUI
+
+AirTurnUI is a user interface for AirTurnInterface. See [https://airturn.com/framework](https://airturn.com/framework) for more info.
