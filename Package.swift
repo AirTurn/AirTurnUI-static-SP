35e69d03a92cd0001f2979d462d5911b9fff87af
Nick Brook <nrbrook@gmail.com>
2026-09-04 10:44:42 +0100

    4.10.0-b.1

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@
 
 import PackageDescription
 
-let version = "4.8.0-b.2"
-let checksum = "eb4aeed16a386933d928d480672f30862ff9a20451e008b9a89a06bcb6044ede"
+let version = "4.10.0-b.1"
+let checksum = "b472f8e2d2ce64fd02442b5185ef9c517f3fec1c0f9b49e939ce1ec89b05ac56"
 
 let package = Package(
     name: "AirTurnUI",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
